603a6e1a8f7750482ea655a2f675ae73dd3f8876
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2022-12-30 14:44:08 +0700

    update dependency
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- src/aswad.cr
+++ src/aswad.cr
@@ -1,5 +1,15 @@
-# TODO: Write documentation for `Aswad`
+require "http/client"
+require "option_parser"
+
 module Aswad
   VERSION = "0.1.0"
+
+  tahun = "2022"
+  bulan = "12"
+  tanggal = "30"
+  idkota = "1609"
+  
+  request = "https://api.myquran.com/v1/sholat/jadwal/#{idkota}/#{tahun}/#{bulan}/#{tanggal}"
+  p! request
   puts "hello world"
 end

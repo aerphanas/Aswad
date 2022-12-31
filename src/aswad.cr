c4ea01071222a4de81ec316b4c15b1c06c8f67c1
aerphanas <muhamadaviv14@gmail.com>
2022-12-31 08:15:06 +0000

    print current data json for jakarta
    Signed-off-by: aerphanas <muhamadaviv14@gmail.com>

--- src/aswad.cr
+++ src/aswad.cr
@@ -2,16 +2,21 @@ require "http/client"
 require "option_parser"
 require "json"
 
-module Aswad
-  VERSION = "0.1.0"
+kota = "jakarta"
 
-  tahun = "2022"
-  bulan = "12"
-  tanggal = "30"
-  idkota = "1609"
-  
-  url = "https://api.myquran.com/v1/sholat/jadwal/#{idkota}/#{tahun}/#{bulan}/#{tanggal}"
-  request = HTTP::Client.get url
-  #p! request.body
-  puts JSON.parse(request.body).to_pretty_json
-end
+urlSholat = "https://api.myquran.com/v1/sholat/"
+urlidkota = "#{urlSholat}kota/cari/#{kota}"
+
+request = HTTP::Client.get urlidkota
+
+times = Time.local
+tahun = times.year
+bulan = times.month
+tanggal = times.day
+
+idkota = JSON.parse(request.body)["data"][0]["id"].to_s
+
+url = "#{urlSholat}jadwal/#{idkota}/#{tahun}/#{bulan}/#{tanggal}"
+#p! request.body
+#p! JSON.parse(request.body)["data"][0]["id"].to_s
+puts JSON.parse(HTTP::Client.get(url).body)["data"]["jadwal"].to_pretty_json

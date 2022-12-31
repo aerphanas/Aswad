23d28f7155bf7abc338842235aecd6933c75fe7c
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2022-12-31 11:26:13 +0000

    print current prayer schedule
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- src/aswad.cr
+++ src/aswad.cr
@@ -2,21 +2,44 @@ require "http/client"
 require "option_parser"
 require "json"
 
-kota = "jakarta"
+class ApiQuran
+    property target
+    getter idkota
 
-urlSholat = "https://api.myquran.com/v1/sholat/"
-urlidkota = "#{urlSholat}kota/cari/#{kota}"
+    def initialize(target : String, kota : String)
+        @target = target
+        @times = Time.local
+        @tahun = @times.year
+        @bulan = @times.month
+        @tanggal = @times.day
+        @url = "https://api.myquran.com/v1/"
+        getIdCity kota 
+    end
+    def getIdCity(kota : String)
+        url = "#{@url}#{@target}/kota/cari/#{kota}"
+        request = HTTP::Client.get url
+        @status = JSON.parse(request.body)["status"].to_s
+        if @status == "true"
+            @idkota = JSON.parse(request.body)["data"][0]["id"]
+        elsif @status == "false"
+            puts "error tidak ada kota dengan nama \"#{kota}\" dalam database"
+            exit 1
+        else
+            puts "error"
+            exit 1
+        end
+        
+    end
+    def getScheduleNow
+        url = "#{@url}#{@target}/jadwal/#{@idkota}/#{@tahun}/#{@bulan}/#{@tanggal}"
+        request = HTTP::Client.get url
+        @jadwal = JSON.parse(request.body)["data"]["jadwal"]
+    end
+end
 
-request = HTTP::Client.get urlidkota
-
-times = Time.local
-tahun = times.year
-bulan = times.month
-tanggal = times.day
-
-idkota = JSON.parse(request.body)["data"][0]["id"].to_s
-
-url = "#{urlSholat}jadwal/#{idkota}/#{tahun}/#{bulan}/#{tanggal}"
-#p! request.body
-#p! JSON.parse(request.body)["data"][0]["id"].to_s
-p! JSON.parse(HTTP::Client.get(url).body)["data"]["jadwal"].to_pretty_json
+cek = ApiQuran.new "sholat", "jakarta"
+waktu = {"imsak", "subuh", "terbit", "dhuha", "dzuhur", "ashar", "maghrib", "isya"}
+puts "berikut adalah jadwal waktu solat untuk #{cek.getScheduleNow["tanggal"]}"
+waktu.each do |x|
+    puts  x + "\t- " + cek.getScheduleNow[x].to_s
+end

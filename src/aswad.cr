904a5916090bf668d8af3dcea0b60da9c5a5deb4
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-01-02 08:50:34 +0000

    add new option for interacting with class
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- src/aswad.cr
+++ src/aswad.cr
@@ -1,51 +1,34 @@
 require "http/client"
 require "option_parser"
 require "json"
+require "html"
 
-class ApiQuran
-  property target
-  getter idkota, jadwal, lokasi
-
-  def initialize(target : String, kota : String)
-    @target = target
-    @times = Time.local
-    @tahun = @times.year
-    @bulan = @times.month
-    @tanggal = @times.day
-    @url = "https://api.myquran.com/v1/"
-    getIdCity kota 
-  end
-
-  def getIdCity(kota : String)
-    url = "#{@url}#{@target}/kota/cari/#{kota}"
-    request = HTTP::Client.get url
-    @status = JSON.parse(request.body)["status"].to_s
-    if @status == "true"
-      @idkota = JSON.parse(request.body)["data"][0]["id"]
-      @lokasi = JSON.parse(request.body)["data"][0]["lokasi"]
-    elsif @status == "false"
-      puts "error tidak ada kota dengan nama \"#{kota}\" dalam database"
-      exit 1
-    else
-      puts "error"
-      exit 1
-    end    
-  end
-
-  def getScheduleNow
-    url = "#{@url}#{@target}/jadwal/#{@idkota}/#{@tahun}/#{@bulan}/#{@tanggal}"
-    request = HTTP::Client.get url
-    @jadwal = JSON.parse(request.body)["data"]["jadwal"]
-  end
-end
+require "./ApiQuran/*"
 
 destination = ""
-waktu = {"imsak", "subuh", "terbit", "dhuha", "dzuhur", "ashar", "maghrib", "isya"}
+surat = ""
+ayat = ""
+target = ""
+waktu = {
+  "imsak", "subuh",
+  "terbit", "dhuha",
+  "dzuhur", "ashar",
+  "maghrib", "isya",
+}
 
 OptionParser.parse do |parser|
   parser.banner = "Usage: aswad [arguments]"
+  parser.on("-t TARGET", "--target=TARGET", "target get Prayer Time or Interpretation") do |choose|
+    target = choose
+  end
   parser.on("-c NAME", "--city=NAME", "the name of the city you want to search for") do |name|
     destination = name
+  end
+  parser.on("-s SURAH", "--surah=SURAH", "surah") do |surah|
+    surat = surah
+  end
+  parser.on("-a AYAH", "--ayah=AYAH", "ayah") do |ayah|
+    ayat = ayah
   end
   parser.on("-h", "--help", "Show this help") do
     puts parser
@@ -63,16 +46,26 @@ OptionParser.parse do |parser|
   end
 end
 
-
-if !destination.empty?
-  get = ApiQuran.new("sholat", destination)
-  jadwals = get.getScheduleNow
-  puts "Jadwal Waktu Sholat untuk #{get.lokasi}\n"
-  waktu.each do |x|
-    puts  x + "\t- " + jadwals[x].to_s
+if !target.empty?
+  case target
+  when "sholat"
+    get = ApiQuran.new("sholat")
+    jadwals = get.getScheduleNow destination
+    puts "Jadwal Waktu Sholat untuk #{get.lokasi}\n"
+    waktu.each do |x|
+      puts x + "\t- " + jadwals[x].to_s
+    end
+    puts "Valid untuk #{jadwals["tanggal"]}\n"
+  when "tafsir"
+    get = ApiQuran.new("tafsir")
+    get.getInterpretation(surat.to_u8, ayat.to_u8)
+    puts "menampilkan tafsir \"#{surat}:#{ayat}\":\n\n"
+    puts HTML.unescape(get.summary.to_s)
+  else
+    puts "Run 'aswad --help' for more information on a command."
+    exit
   end
-  puts "Valid untuk #{jadwals["tanggal"]}\n"
 else
   puts "Run 'aswad --help' for more information on a command."
-  exit 
+  exit
 end
